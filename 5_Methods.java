815aa003dcf46f7d91c303f771e8a4be67876bea
ManuelBasso <146716888+ManuelBasso@users.noreply.github.com>
2023-10-10 14:24:39 +0200

    Add files via upload

---
+++ 5_Methods.java
@@ -0,0 +1,19 @@
+//Definire una funzione che accetti in ingresso 2 interi e ne restituisca la somma.
+
+public class Main {
+    public static void main(String args[]) {
+    int first = 4;
+    int second = 2;
+    
+    System.out.println("Somma = " + sumOf (first, second) );
+    
+    }
+    
+    
+    public static int sumOf (int a , int b) {
+    int sum = a + b;
+    return sum;
+ }
+    
+}
+
